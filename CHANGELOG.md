99fd20030d25c16868d8ffce8a7e428f42380fa8
Jawad <cp@bhc.gov.pk>
2020-04-23 14:18:15 +0500

    updated dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2020-03-21
+
+- updated dependencies
+
 ## [1.0.0] - 2020-03-21
 
 - version number as stable.
--- example/android/app/src/main/kotlin/pro/jawad/pdfviewerjkexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/pro/jawad/pdfviewerjkexample/MainActivity.kt
@@ -1,12 +1,4 @@
 package pro.jawad.pdfviewerjkexample
-
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
